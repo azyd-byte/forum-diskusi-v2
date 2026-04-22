fb3fb180f11bce6c2464a0b4b758c2ea091cfecc
zayadi <birulauthijaualam@gmail.com>
2026-04-22 10:40:04 +0700

    make test pass

--- src/pages/LoginPage.test.jsx
+++ src/pages/LoginPage.test.jsx
@@ -7,13 +7,6 @@ jest.mock("react-redux", () => ({
   useDispatch: jest.fn(),
 }));
 
-// jest.mock("react-router-dom", () => {
-//   return {
-//     useNavigate: () => jest.fn(),
-//     Link: ({ children }) => children,
-//   };
-// });
-
 /**
  * test scenarios for LoginPage
  *
@@ -79,7 +72,7 @@ describe("LoginPage component", () => {
     expect(mockNavigate).toHaveBeenCalledWith("/");
   });
 
-  it("dummy test for CI error", () => {
-    expect(true).toBe(false);
-  });
+  // it("dummy test for CI error", () => {
+  //   expect(true).toBe(false);
+  // });
 });
